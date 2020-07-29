7f351b48965dd08f9fb40dd8ba9830e03afbebc8
prabhakarzx <ppandey2098@gmail.com>
2020-07-30 01:27:57 +0530

    Fix linters

--- Custom_Enumerable_Methods.rb
+++ Custom_Enumerable_Methods.rb
@@ -1,35 +1,37 @@
 module Enumerable
   def my_each
+    return to_enum(:my_each) unless block_given?
     i = 0
     while i < self.size
       yield(self[i])
       i += 1
     end
-  end
+  end  
 
   def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
     for i in i...self.size
       yield(self[i], i)
     end
-  end
+  end  
 
   def my_select
-   result = []
-   self.my_each { |i| result.push(i) if yield(i) }
-   result
-  end
+    result = []
+    self.my_each { |i| result.push(i) if yield(i) }
+    result
+  end  
 
   def my_all?
-  result = true
-  self.my_each { |i| false unless yield(i) }
-  result
-  end 
+    result = true
+    self.my_each { |i| false unless yield(i) }
+    result
+  end   
 
   def my_any?
     result = false
     self.my_each { |i| false unless yield(i) }
     result
-  end
+  end  
 
   def my_none?
     result = true
@@ -37,68 +39,62 @@ module Enumerable
       result = false if self[i] == yield(i)
     end
     result
-  end
+  end  
 
   def my_count
     i = 0
     self.my_each { i += 1 }
     i
-    end 
+  end   
 
   def my_map
-    return to_enum(:my_map) unless block_given?
-
+    return to_enum(:my_map) unless block_given?  
     new_self = []
     my_each do |element|
       new_self << if is_a? Hash
         yield(element[0], element[1])
       else
-      yield(element)
-          end
+        yield(element)
+      end
     end
     new_self
-  end
+  end  
 
   def my_inject (initial = 0)
     i = 0
     accumulator = initial
     while (i < self.length)
       accumulator = yield(accumulator, self[i])
-      i = i + 1
+      i += 1
     end
     accumulator
-  end
+  end  
 
   def my_map_with_proc(&proc)
-    return to_enum(:my_map_with_proc) unless block_given?
-
+    return to_enum(:my_map_with_proc) unless block_given?  
     new_self = []
     my_each do |element|
       new_self << if is_a? Hash
-                    proc.call(element[0], element[1])
-                  else
-                    proc.call(element)
-                  end
-    end
-
+        proc.call(element[0], element[1])
+      else
+        proc.call(element)
+      end
+    end  
     new_self
-  end
+  end  
 
   def my_map_with_proc_or_block(&proc)
-    return to_enum(:my_map_with_proc_or_block) unless block_given?
-
+    return to_enum(:my_map_with_proc_or_block) unless block_given?  
     new_self = []
     my_each do |element|
       new_self << if is_a? Hash
-                    (proc ? proc.call(element[0], element[1]) : yield(element[0], element[1]))
-                  else
-                    (proc ? proc.call(element) : yield(element))
-                  end
-    end
-
+        (proc ? proc.call(element[0], element[1]) : yield(element[0], element[1]))
+      else
+        (proc ? proc.call(element) : yield(element))
+      end
+    end  
     new_self
-  end
-
+  end  
 end
 
 def multiply_els(arr)
@@ -106,4 +102,3 @@ def multiply_els(arr)
 end
 
 p multiply_els([2, 4, 5])
-
